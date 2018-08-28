65601f92373953f70d9689b2cc2eabaaba91f046
croc <leymarien@gmail.com>
2018-08-28 15:44:25 +0200

    first commit

---
+++ js/global.js
@@ -0,0 +1,19 @@
+// Appel du prototype Diaporama objet
+var Diaporama1 = Object.create(Diaporama);
+Diaporama1.init;
+
+// Appel du prototype Maps objet
+var Maps1 = Object.create(Maps);
+Maps1.init;
+
+// Appel du prototype Station objet
+var Station1 = Object.create(Station);
+Station1.init;
+
+// Appel du prototype Compteur objet
+var reservation = Object.create(Compteur);
+reservation.init;
+
+// Appel du prototype Signature objet
+var Signature1 = Object.create(Signature);
+Signature1.init;
